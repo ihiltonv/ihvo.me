80228b8ada7e7ed208aa0681d20e7140bbf0cf25
Isaac Hilton-VanOsdall <isaac_hilton-vanosdall@brown.edu>
2020-12-04 17:44:53 -0500

    added title

--- pages/index.js
+++ pages/index.js
@@ -5,21 +5,26 @@ import '../styles/globals.css'
 
 export default function Home() {
   return (
-    <div className="container mx-auto pt-8">
-      <h1 className="text-2xl font-bold text-center">Isaac Hilton-VanOsdall</h1>
-      <h3 className="text-xl font-bold">Some projects I've worked on recently:</h3>
-      <Link href="/projects/development">
-        <a className="text-md text-blue-500 cursor-auto block">React Development</a>
-      </Link>
-      <Link href="/projects/iterative-design">
-        <a className="text-md text-blue-500 cursor-auto block">Iterative Design: Hellosaurus</a>
-      </Link>
-      <Link href="/projects/responsive-redesign">
-        <a className="text-md text-blue-500 cursor-auto block">Responsive Redesign: Ormsbys</a>
-      </Link>
-      <Link href="/projects/ab-testing">
-        <a className="text-md text-blue-500 cursor-auto block">A/B Testing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Isaac Hilton-VanOsdall</title>
+      </Head>
+      <div className="container mx-auto pt-8">
+        <h1 className="text-2xl font-bold text-center">Isaac Hilton-VanOsdall</h1>
+        <h3 className="text-xl font-bold">Some projects I've worked on recently:</h3>
+        <Link href="/projects/development">
+          <a className="text-md text-blue-500 cursor-auto block">React Development</a>
+        </Link>
+        <Link href="/projects/iterative-design">
+          <a className="text-md text-blue-500 cursor-auto block">Iterative Design: Hellosaurus</a>
+        </Link>
+        <Link href="/projects/responsive-redesign">
+          <a className="text-md text-blue-500 cursor-auto block">Responsive Redesign: Ormsbys</a>
+        </Link>
+        <Link href="/projects/ab-testing">
+          <a className="text-md text-blue-500 cursor-auto block">A/B Testing</a>
+        </Link>
+      </div>
+    </>
   )
 }
